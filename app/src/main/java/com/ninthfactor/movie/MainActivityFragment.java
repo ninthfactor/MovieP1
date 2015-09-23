fbbbfa8038451956182b0a6d0f8e0f87b9e3c13d
Ninth Factor <ninthfactor@gmail.com>
2015-09-23 14:26:59 -0400

    created grid view with sample images

--- app/src/main/java/com/ninthfactor/movie/MainActivityFragment.java
+++ app/src/main/java/com/ninthfactor/movie/MainActivityFragment.java
@@ -1,10 +1,11 @@
 package com.ninthfactor.movie;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +18,12 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridview = (GridView) rootview.findViewById(R.id.gridview_summary);
+        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+
+        return rootview;
     }
 }
